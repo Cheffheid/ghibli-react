d61a0290cefe6450918c79232d4f68ad296fb22a
Jeffrey de Wit <jeffrey@webdevstudios.com>
2017-05-02 22:45:29 -0400

    Set up a header, homepage links

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+import Header from '../components/Header';
 
 class Homepage extends React.Component {
 
 	render() {
 		return (
-			<div>
-				Homepage
-			</div>
+			<section>
+				<Header />
+				<div className="linkbox">
+					<Link to="/films">Films</Link>
+				</div>
+				<div className="linkbox">
+					<Link to="/people">People</Link>
+				</div>
+				<div className="linkbox">
+					<Link to="/locations">Locations</Link>
+				</div>
+				<div className="linkbox">
+					<Link to="/species">Species</Link>
+				</div>
+				<div className="linkbox">
+					<Link to="/vehicles">Vehicles</Link>
+				</div>
+			</section>
 		)
 	}
 
